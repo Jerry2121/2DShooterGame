c9eb109de0e9bd33392cf4c9ba761b7bfed5311f
Jeremiah Templeton <19.jeremiah.templeto@ksd.org>
2018-06-04 10:38:26 -0700

    rotation

---
+++ 2DShooterGameMobile/Assets/Scripts/movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movement : MonoBehaviour {
+
+	public float Movement_Speed = 3.0f;
+    //  public GameObject Timertext;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//horizontal stores the user input when they press
+		//a, d, left, right on a scale from -1 to 1
+		float horizontal = Input.GetAxis("Horizontal");
+		//vertical stores the user input when they press
+		//w, s, up, down on a scale from -1 to 1
+		float vertical = Input.GetAxis ("Vertical");
+		//create a puish variable that combines the user input
+		Vector2 push = new Vector2 (horizontal, vertical);
+		//use push to change the velocity of the object
+		gameObject.GetComponent<Rigidbody2D> ().velocity = push * Movement_Speed;
+	}
+}
---
+++ 2DShooterGameMobile/Assets/Virtual Joystick Pack/Example/PlayerExample.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerExample : MonoBehaviour {
+
+    public float moveSpeed;
+    public Joystick joystick;
+    public Joystick joystick2;
+    public Vector3 joyStartPos;
+    public GameObject handle;
+    private void Start()
+    {
+        joyStartPos = handle.transform.position;
+    }
+    void Update () 
+	{
+        Vector3 moveVector = (transform.right * joystick.Horizontal + transform.up * joystick.Vertical).normalized;
+        transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+        Vector3 rotateVector = (transform.right * joystick2.Horizontal*180 + transform.up * joystick2.Vertical*180);
+        /*if x < 0 y == 0
+         * looking right
+         * rotate 90 degrees clockwise
+         * if x > 0 y == 0
+         * looking left
+         * rotate 90 degrees counterclockwise
+         * if y < 0
+         * looking down
+         * rotate 180 degrees
+         * if y > 0
+         * looking up
+         * rotate 0 degrees
+         * 
+         * */
+        Vector3 foo = (handle.transform.position - joyStartPos).normalized;
+        if (foo.magnitude == 1)
+        {
+            transform.forward = foo;
+            transform.Rotate(new Vector3(0, 90, 90));
+            //transform.rotation = Quaternion.Euler(rotateVector);
+
+
+        }
+    }
+}
